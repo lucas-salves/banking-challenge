eba6a158aa0e9e545f0f4e86eb9d4dedaf815213
lucas-salves <salves.lucas@outlook.com>
2022-02-27 00:56:19 -0300

    Initial commit

---
+++ src/main/java/br/stark/invoicecontroller/InvoiceControllerApplication.java
@@ -0,0 +1,13 @@
+package br.stark.invoicecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceControllerApplication.class, args);
+	}
+
+}
