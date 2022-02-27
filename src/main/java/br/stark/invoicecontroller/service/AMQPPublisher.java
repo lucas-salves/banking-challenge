4d79489866a9f9d71979c9f4a9cca8c661bf85c8
lucas-salves <salves.lucas@outlook.com>
2022-02-27 01:15:21 -0300

    Add the AMQPublisher service class
    Add the RabbitMQ service

---
+++ src/main/java/br/stark/invoicecontroller/service/AMQPPublisher.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.stark.invoicecontroller.service;
+
+import br.stark.invoicecontroller.config.AMQPConfig;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lucas
+ */
+public class AMQPPublisher {
+
+    private Connection connection;
+
+    private Channel channel;
+
+    public AMQPPublisher() {
+        try {
+            connection = AMQPConfig.getConnection();
+            channel = connection.createChannel();
+        } catch (IOException ex) {
+            Logger.getLogger(AMQPPublisher.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sendToQueue(String queue, String message) throws IOException {
+        if (connection == null || channel == null) {
+
+        }
+
+        channel.queueDeclare(queue, true, false, false, null);
+    }
+
+    public void close() {
+        //replace with a try-with-resources later
+        try {
+            channel.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
